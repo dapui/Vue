7523ba7521b9c6827c49b74c7c73a3154406a8d5
Dapui <odh550@naver.com>
2025-07-21 00:08:28 +0900

    반응형 기초(Reactivity)

---
+++ learn-vue3/src/lectures/7/App_ref_object_array.vue
@@ -0,0 +1,31 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { reactive, ref } from "vue";
+
+export default {
+  setup() {
+    // ref -> Object
+    const count = ref(0);
+    const state = reactive({
+      count, // [count: count -> count] 키와 값이 같으면 단축속성명으로 표현 가능
+    });
+
+    count.value++;
+    count.value++;
+    console.log(count.value);
+    console.log("state.count: ", state.count);
+
+    // ref -> Array
+    const message = ref("Hello");
+    const arr = reactive([message]);
+    console.log("arr[0]: ", arr[0].value);
+
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ learn-vue3/src/lectures/7/App_toRefs_toRef.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>author: {{ author }}</p>
+    <p>title: {{ title }}</p>
+  </div>
+</template>
+
+<script>
+import { reactive, toRef } from "vue";
+
+export default {
+  setup() {
+    const book = reactive({
+      author: "Vue Team",
+      year: "2020",
+      title: "Vue 3 Guide",
+      description: "당신은 지금 바로 이 책을 읽습니다 ;)",
+      price: "무료",
+    });
+
+    // 반응형 객체에서 속성을 가져오고 싶을 때, 반응형을 잃지 않고 가져오려면 toRefs 또는 toRef를 사용
+    // const { author, title } = toRefs(book);
+    const author = toRef(book, "author");
+    const title = toRef(book, "title");
+
+    return { author, title, book };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
