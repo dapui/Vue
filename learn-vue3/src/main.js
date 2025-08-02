f8828cde14ebff24a76413854bfbd94497e4390c
Dapui <odh550@naver.com>
2025-08-02 21:56:26 +0900

    컴포넌트 기초

--- learn-vue3/src/main.js
+++ learn-vue3/src/main.js
@@ -1,6 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { createApp } from "vue";
 import App from "./App.vue";
+//import AppCard from "./components/AppCard.vue"; // 전역 컴포넌트로 등록시 사용
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+// app.component('AppCard', AppCard); // 전역 컴포넌트로 등록시 사용
+
+app.mount("#app");
 import "bootstrap/dist/js/bootstrap.js";
