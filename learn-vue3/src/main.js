e995876b8fa1c17830e1369305293d4689113dfd
Dapui <odh550@naver.com>
2025-08-13 23:16:11 +0900

    Provide / Inject

--- learn-vue3/src/main.js
+++ learn-vue3/src/main.js
@@ -7,5 +7,10 @@ const app = createApp(App);
 
 // app.component('AppCard', AppCard); // 전역 컴포넌트로 등록시 사용
 
+app.provide("app-message", "app message 입니다.");
+
+app.config.globalProperties.msg = "hello"; // Vue3에서는 setup() 내에서 this로 접근할 수 없음
+app.provide("msg", "hello msg");
+
 app.mount("#app");
 import "bootstrap/dist/js/bootstrap.js";
