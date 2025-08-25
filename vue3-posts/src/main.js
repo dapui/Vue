9c72d486cc287219f66e6a72109546f0e9d63bfa
Dapui <odh550@naver.com>
2025-08-25 12:54:25 +0900

    axios 모듈 & Vite 환경 변수 설정 (env)

--- vue3-posts/src/main.js
+++ vue3-posts/src/main.js
@@ -1,9 +1,16 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
 createApp(App).use(router).mount('#app');
-// import 'bootstrap/dist/js/bootstrap.js'; 
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+// import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// console.log('MODE', import.meta.env.MODE);
+// console.log('BASE_URL', import.meta.env.BASE_URL);
+// console.log('PROD', import.meta.env.PROD);
+// console.log('DEV', import.meta.env.DEV);
+// console.log('SSR', import.meta.env.SSR);
+// console.log('VITE_APP_API_URL', import.meta.env.VITE_APP_API_URL);
