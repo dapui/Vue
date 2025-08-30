76b64c177cb8edcb74a9cc1da0182a13acce0446
Dapui <odh550@naver.com>
2025-08-31 02:38:05 +0900

    Teleport 컴포넌트 : Modal 만들기

--- vue3-posts/src/views/posts/PostListView.vue
+++ vue3-posts/src/views/posts/PostListView.vue
@@ -14,19 +14,23 @@
           :content="item.content"
           :created-at="item.createdAt"
           @click="goPage(item.id)"
+          @modal="openModal(item)"
         ></PostItem>
       </template>
     </AppGrid>
-    <div class="row g-3">
-      <div v-for="post in posts" :key="post.id" class="col-4">
-
-      </div>
-    </div>
     <AppPagination
       :current-page="params._page"
       :page-count="pageCount"
       @page="page => (params._page = page)"
     />
+    <Teleport to="#modal">
+      <PostModal
+        v-model="show"
+        :title="modalTitle"
+        :content="modalContent"
+        :createAt="modalCreateAt"
+      />
+    </Teleport>
     <template v-if="posts && posts.length > 0">
       <hr class="my-5" />
       <AppCard>
@@ -40,6 +44,7 @@
 import PostItem from '@/components/posts/PostItem.vue';
 import PostDetailView from '@/views/posts/PostDetailView.vue';
 import PostFilter from '@/components/posts/PostFilter.vue';
+import PostModal from '@/components/posts/PostModal.vue';
 import AppCard from '@/components/AppCard.vue';
 import AppGrid from '@/components/AppGrid.vue';
 import AppPagination from '@/components/AppPagination.vue';
@@ -84,6 +89,18 @@ const goPage = id => {
       id,
     },
   });
+};
+
+// modal
+const show = ref(false);
+const modalTitle = ref('');
+const modalContent = ref('');
+const modalCreateAt = ref('');
+const openModal = ({ title, content, createdAt }) => {
+  show.value = true;
+  modalTitle.value = title;
+  modalContent.value = content;
+  modalCreateAt.value = createdAt;
 };
 </script>
 
